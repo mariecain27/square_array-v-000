4184277f241ddecbdfcf93ec5e55a3898693431d
Marie Cain <mariecain27@gmail.com>
2018-05-30 02:20:26 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,8 @@
 #def square_array(array)
- # new_array = [ ]
-#  array.each do |number|
-#  new_array << number ** 2 
-#  end
-#  new_array
-# end
-
-def square_array(array)
-  array.collect { |number| number ** 2 }
-  array
+  new_array = [ ]
+ array.each do |number|
+  new_array << number ** 2 
+  end
+  new_array
 end
+
